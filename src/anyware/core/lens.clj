09b4c82dee43662b4515e3b65c0f03a431c00b12
Sanshiro Yoshida <halcat0x15a@gmail.com>
2013-03-10 18:50:19 +0900

    only keyword

--- src/anyware/core/lens.clj
+++ src/anyware/core/lens.clj
@@ -2,28 +2,26 @@
   (:refer-clojure :exclude [get set comp name])
   (:require [clojure.zip :as zip]))
 
-(defrecord Lens [get set])
+(deftype Lens [get set])
 
-(defmulti get (fn [lens _] (type lens)))
-(defmethod get Lens [lens obj] ((:get lens) obj))
-(defmethod get :default [lens obj] (lens obj))
+(def zip (Lens. zip/node zip/replace))
 
-(defmulti set (fn [lens _ _] (type lens)))
-(defmethod set Lens [lens value obj] ((:set lens) obj value))
-(defmethod set :default [lens value obj] (assoc obj lens value))
+(defn get [lens obj]
+  (if (keyword? lens)
+    (lens obj)
+    ((.-get lens) obj)))
+
+(defn set [lens value obj]
+  (if (keyword? lens)
+    (assoc obj lens value)
+    ((.-set lens) obj value)))
 
 (defn modify [lens f obj]
   (set lens (f (get lens obj)) obj))
 
 (defn comp [lens' lens]
-  (Lens. (fn [obj]
-           (->> obj
-                (get lens)
-                (get lens')))
-         (fn [obj value]
-           (modify lens (partial set lens' value) obj))))
-
-(def zip (Lens. zip/node zip/replace))
+  (Lens. (fn [obj] (->> obj (get lens) (get lens')))
+         (fn [obj value] (modify lens (partial set lens' value) obj))))
 
 (def entry (comp zip :list))
 
