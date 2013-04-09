c27be013824f81f0f576b247f2499b4ac9e26eca
Sanshiro Yoshida <halcat0x15a@gmail.com>
2013-04-09 14:57:59 +0900

    fix html escapement

--- src/anyware/core/format.clj
+++ src/anyware/core/format.clj
@@ -9,7 +9,6 @@
   (text [format text]))
 
 (defn write [format x]
-  (prn x)
   (cond (:label x) (node format x)
         (vector? x) (reduce str (mapv (partial write format) x))
         :else (text format (str x))))
@@ -17,4 +16,4 @@
 (defn render [format editor]
   (root format
         (str (->> editor (record/get record/buffer) ast/parse (write format))
-             (->> editor (record/get record/minibuffer) buffer/write))))
+             (->> editor (record/get record/minibuffer) buffer/write (text format)))))
